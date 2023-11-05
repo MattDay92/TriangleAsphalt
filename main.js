dcee8e8c30f1f4848294cc1a7fffee4cbed760ae
Matt Day <mattday@matts-mac-mini.lan>
2023-11-05 15:34:45 -0500

    Updated colors and LinkedIN BTN

--- main.js
+++ main.js
@@ -20,11 +20,16 @@ const changeNavSize = () => {
         document.getElementById("logo").style = "height:10vh"
         document.querySelector("nav").style = "height:fit-content"
         document.getElementById("BackToTopBTN").style = "opacity:1"
+        document.getElementById("LinkedInBTN").style = "opacity:1"
     } else {
         document.getElementById("nav-logo-small").style = "opacity:0"
         document.getElementById("logo").style = "height:0vh"
         document.querySelector("nav").style = "height:0vh"
         document.getElementById("BackToTopBTN").style = "opacity:0"
-
+        document.getElementById("LinkedInBTN").style = "opacity:0"
     }
+}
+
+const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
 }
